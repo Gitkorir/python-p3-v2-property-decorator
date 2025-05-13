a515f6b6b8adcbd070e88a80012dbe1054d0af86
Arnold <arnoldkorir201@gmail.com>
2025-05-13 23:19:12 +0300

    exerside on decorator def methods

--- lib/dog.py
+++ lib/dog.py
@@ -14,10 +14,10 @@ class Dog:
     def __init__(self, name='Fido', breed='Mastiff'):
         self.name = name
         self.breed = breed
-
+    @property
     def get_name(self):
         return self._name
-
+    @name.setter 
     def set_name(self, name):
         if isinstance(name, str) and 1 <= len(name) <= 25:
             self._name = name.title()
@@ -26,10 +26,10 @@ class Dog:
                 "Name must be string between 1 and 25 characters.")
 
     name = property(get_name, set_name)
-
+    @property
     def get_breed(self):
         return self._breed
-
+    @breed.setter
     def set_breed(self, breed):
         if breed in APPROVED_BREEDS:
             self._breed = breed
